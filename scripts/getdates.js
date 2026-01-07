e457d02ef62e0f92e0bc83b7243531dc747db312
ogustiien <aogubie@byupathway.edu>
2026-01-07 15:09:38 +0200

    week1

---
+++ scripts/getdates.js
@@ -0,0 +1,10 @@
+const lastmodified = document.querySelector("#lastmodified");
+
+const today = new Date();
+
+full.innerHTML = `Today is <span class="lastmodified"> ${new Intl.DateTimeFormat(
+    "en-US",
+    {
+        dateStyle: "full"
+    }
+).format(today)}</span>`;
