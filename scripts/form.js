36ae1246d82a6050ee955d797e442e97d803d7cd
ogustiien <aogubie@byupathway.edu>
2026-02-11 15:39:16 +0200

    spelling

--- scripts/form.js
+++ scripts/form.js
@@ -1,27 +1,28 @@
 const products = [
   {
     id: "fc-1888",
-    name: "flux capacitor",
+    name: "Flux capacitor",
     averagerating: 4.5
   },
   {
+    id: "ac-2000",
+    name: "Low voltage reactor",
+    averagerating: 3.9
+  },
+  {
     id: "fc-2050",
-    name: "power laces",
+    name: "Power laces",
     averagerating: 4.7
   },
   {
     id: "fs-1987",
-    name: "time circuits",
+    name: "Time circuits",
     averagerating: 3.5
   },
-  {
-    id: "ac-2000",
-    name: "low voltage reactor",
-    averagerating: 3.9
-  },
+
   {
     id: "jj-1969",
-    name: "warp equalizer",
+    name: "Warp equalizer",
     averagerating: 5.0
   }
 ];
